d03f9850b14d37738f43956d9d8d4bad82eb1e50
Akshay-Achuthan <ithub97@gmail.com>
2022-04-17 23:31:01 +0530

    updated

---
+++ index.js
@@ -0,0 +1,7 @@
+// var btn = document.querySelector(".nav__btn");
+
+// btn.addEventListener("click", function () {
+//   btn.classList.toggle("side-bar");
+//   btn.style.transform = "translateX(0%)";
+//   console.log("clicked");
+// });
